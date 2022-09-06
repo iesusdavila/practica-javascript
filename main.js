f771e6f17ba1a592725da2939b020e993650b986
iesusdavila <irdavila@espol.edu.ec>
2022-09-05 23:36:24 -0500

    Implementada la interfaz de carrito de compras evitando superposicion de ventanas

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 const desktop_menu = document.querySelector(".desktop-menu");
 const navbar_email = document.querySelector(".navbar-email");
+const mobile_menu = document.querySelector(".mobile-menu");
+const menu = document.querySelector(".menu");
+const product_detail = document.querySelector(".product-detail");
+const navbar_shopping_cart = document.querySelector(".navbar-shopping-cart");
 
-navbar_email.addEventListener("click", () =>
-  desktop_menu.classList.toggle("inactive")
-);
+navbar_email.addEventListener("click", () => {
+  if (!product_detail.classList.contains("inactive")) {
+    product_detail.classList.add("inactive");
+  }
+  desktop_menu.classList.toggle("inactive");
+});
+
+menu.addEventListener("click", () => {
+  if (!product_detail.classList.contains("inactive")) {
+    product_detail.classList.add("inactive");
+  }
+  mobile_menu.classList.toggle("inactive");
+});
+
+navbar_shopping_cart.addEventListener("click", () => {
+  if (!desktop_menu.classList.contains("inactive")) {
+    desktop_menu.classList.add("inactive");
+  }
+  if (!mobile_menu.classList.contains("inactive")) {
+    mobile_menu.classList.add("inactive");
+  }
+  product_detail.classList.toggle("inactive");
+});
